d913183065ebe52352619a22ea97917f6c5326db
billieweisiger <58191447+billieweisiger@users.noreply.github.com>
2021-03-30 22:29:29 -1000

    Create animation2.js

---
+++ animation2.js
@@ -0,0 +1,29 @@
+var tl = anime.timeline({
+  easing: 'easeOutExpo',
+  duration: 1000,
+  delay: 1000
+});
+
+tl
+  .add({
+    targets: '#intro1',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutBack",
+    delay: 0
+  })
+  .add({
+    targets: '#intro2',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutBack",
+    delay: 0
+  })
+  .add({
+    targets: '#intro3',
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutBack"
+  });
